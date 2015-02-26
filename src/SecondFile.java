220f95902a934a471c8c2635e3cff4a6a6869019
jue-henry <jue-henry@apcs.balboa.sfusd.edu>
2015-02-26 15:02:02 -0500

    Second Commit

--- src/SecondFile.java
+++ src/SecondFile.java
@@ -1,11 +1,9 @@
 
 public class SecondFile {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("second File");
 
 	}
 
